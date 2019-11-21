f40f2cdefd77eeeea7c73746b1e2928a6429cd1e
Yakir <yakir.helets@gmail.com>
2019-11-22 00:02:00 +0200

    update dataloader.py

--- hw1/hw1/dataloaders.py
+++ hw1/hw1/dataloaders.py
@@ -33,14 +33,17 @@ def create_train_validation_loaders(dataset: Dataset, validation_ratio,
     num_of_train_samples = num_of_samples - num_of_valid_samples
 
     lengths = [num_of_train_samples, num_of_valid_samples]
-    train_dataset, valid_dataset = torch.utils.data.random_split(dataset, lengths)
+    # train_dataset, valid_dataset = torch.utils.data.random_split(dataset, lengths)
 
-    train_sampler = sampler.SubsetRandomSampler(train_dataset)
-    valid_sampler = sampler.SubsetRandomSampler(valid_dataset)
+    # train_sampler = sampler.SubsetRandomSampler(train_dataset)
+    # valid_sampler = sampler.SubsetRandomSampler(valid_dataset)
 
-    dl_train = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=0, shuffle=False, sampler=train_sampler)
-    dl_valid = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=batch_size, num_workers=0, shuffle=False, sampler=valid_sampler)
-
+    # dl_train = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=0, shuffle=False, sampler=train_sampler)
+    # dl_valid = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=batch_size, num_workers=0, shuffle=False, sampler=valid_sampler)
+    numbers = list(range(0, num_of_train_samples)) 
+    dl_train = torch.utils.data.DataLoader(dataset,batch_size=batch_size,num_workers=num_workers, sampler=torch.utils.data.SubsetRandomSampler(numbers)
+    numbers = list(range(num_of_train_samples+1, num_of_samples))
+    dl_valid = torch.utils.data.DataLoader(dataset,batch_size=batch_size,num_workers=num_workers, sampler=torch.utils.data.SubsetRandomSampler(numbers2)
     # ========================
 
     return dl_train, dl_valid
