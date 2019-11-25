5203e50ad4839ca4f999feb00c55fcd1042932b8
Yakir <yakir.helets@gmail.com>
2019-11-25 10:28:37 +0200

    Upload hw2 initial files

---
+++ hw2/hw2/cnn.py
@@ -0,0 +1,160 @@
+import torch
+import itertools as it
+import torch.nn as nn
+
+
+class ConvClassifier(nn.Module):
+    """
+    A convolutional classifier model based on PyTorch nn.Modules.
+
+    The architecture is:
+    [(CONV -> ReLU)*P -> MaxPool]*(N/P) -> (Linear -> ReLU)*M -> Linear
+    """
+
+    def __init__(self, in_size, out_classes: int, channels: list,
+                 pool_every: int, hidden_dims: list):
+        """
+        :param in_size: Size of input images, e.g. (C,H,W).
+        :param out_classes: Number of classes to output in the final layer.
+        :param channels: A list of of length N containing the number of
+            (output) channels in each conv layer.
+        :param pool_every: P, the number of conv layers before each max-pool.
+        :param hidden_dims: List of of length M containing hidden dimensions of
+            each Linear layer (not including the output layer).
+        """
+        super().__init__()
+        assert channels and hidden_dims
+
+        self.in_size = in_size
+        self.out_classes = out_classes
+        self.channels = channels
+        self.pool_every = pool_every
+        self.hidden_dims = hidden_dims
+
+        self.feature_extractor = self._make_feature_extractor()
+        self.classifier = self._make_classifier()
+
+    def _make_feature_extractor(self):
+        in_channels, in_h, in_w, = tuple(self.in_size)
+
+        layers = []
+        # TODO: Create the feature extractor part of the model:
+        #  [(CONV -> ReLU)*P -> MaxPool]*(N/P)
+        #  Use only dimension-preserving 3x3 convolutions. Apply 2x2 Max
+        #  Pooling to reduce dimensions after every P convolutions.
+        #  Note: If N is not divisible by P, then N mod P additional
+        #  CONV->ReLUs should exist at the end, without a MaxPool after them.
+        # ====== YOUR CODE: ======
+        raise NotImplementedError()
+
+        # ========================
+        seq = nn.Sequential(*layers)
+        return seq
+
+    def _make_classifier(self):
+        in_channels, in_h, in_w, = tuple(self.in_size)
+
+        layers = []
+        # TODO: Create the classifier part of the model:
+        #  (Linear -> ReLU)*M -> Linear
+        #  You'll first need to calculate the number of features going in to
+        #  the first linear layer.
+        #  The last Linear layer should have an output dim of out_classes.
+        # ====== YOUR CODE: ======
+        raise NotImplementedError()
+        # ========================
+        seq = nn.Sequential(*layers)
+        return seq
+
+    def forward(self, x):
+        # TODO: Implement the forward pass.
+        #  Extract features from the input, run the classifier on them and
+        #  return class scores.
+        # ====== YOUR CODE: ======
+        raise NotImplementedError()
+        # ========================
+        return out
+
+
+class ResidualBlock(nn.Module):
+    """
+    A general purpose residual block.
+    """
+
+    def __init__(self, in_channels: int, channels: list, kernel_sizes: list,
+                 batchnorm=False, dropout=0.):
+        """
+        :param in_channels: Number of input channels to the first convolution.
+        :param channels: List of number of output channels for each
+        convolution in the block. The length determines the number of
+        convolutions.
+        :param kernel_sizes: List of kernel sizes (spatial). Length should
+        be the same as channels. Values should be odd numbers.
+        :param batchnorm: True/False whether to apply BatchNorm between
+        convolutions.
+        :param dropout: Amount (p) of Dropout to apply between convolutions.
+        Zero means don't apply dropout.
+        """
+        super().__init__()
+        assert channels and kernel_sizes
+        assert len(channels) == len(kernel_sizes)
+        assert all(map(lambda x: x % 2 == 1, kernel_sizes))
+
+        self.main_path, self.shortcut_path = None, None
+
+        # TODO: Implement a generic residual block.
+        #  Use the given arguments to create two nn.Sequentials:
+        #  the main_path, which should contain the convolution, dropout,
+        #  batchnorm, relu sequences, and the shortcut_path which should
+        #  represent the skip-connection.
+        #  Use convolutions which preserve the spatial extent of the input.
+        #  For simplicity of implementation, we'll assume kernel sizes are odd.
+        # ====== YOUR CODE: ======
+        raise NotImplementedError()
+        # ========================
+
+    def forward(self, x):
+        out = self.main_path(x)
+        out += self.shortcut_path(x)
+        out = torch.relu(out)
+        return out
+
+
+class ResNetClassifier(ConvClassifier):
+    def __init__(self, in_size, out_classes, channels, pool_every,
+                 hidden_dims):
+        super().__init__(in_size, out_classes, channels, pool_every,
+                         hidden_dims)
+
+    def _make_feature_extractor(self):
+        in_channels, in_h, in_w, = tuple(self.in_size)
+
+        layers = []
+        # TODO: Create the feature extractor part of the model:
+        #  [-> (CONV -> ReLU)*P -> MaxPool]*(N/P)
+        #   \------- SKIP ------/
+        #  Use only dimension-preserving 3x3 convolutions. Apply 2x2 Max
+        #  Pooling to reduce dimensions after every P convolutions.
+        #  Note: If N is not divisible by P, then N mod P additional
+        #  CONV->ReLUs (with a skip over them) should exist at the end,
+        #  without a MaxPool after them.
+        # ====== YOUR CODE: ======
+        raise NotImplementedError()
+        # ========================
+        seq = nn.Sequential(*layers)
+        return seq
+
+
+class YourCodeNet(ConvClassifier):
+    def __init__(self, in_size, out_classes, channels, pool_every,
+                 hidden_dims):
+        super().__init__(in_size, out_classes, channels, pool_every,
+                         hidden_dims)
+
+    # TODO: Change whatever you want about the ConvClassifier to try to
+    #  improve it's results on CIFAR-10.
+    #  For example, add batchnorm, dropout, skip connections, change conv
+    #  filter sizes etc.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
