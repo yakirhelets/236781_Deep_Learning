7816668f75ad0fd4b8ea7c06057c4ed931d14ef3
Yakir <yakir.helets@gmail.com>
2019-12-20 17:40:31 +0200

    Chage hyperparams

--- hw2/hw2/answers.py
+++ hw2/hw2/answers.py
@@ -26,7 +26,11 @@ def part2_optim_hp():
     # TODO: Tweak the hyperparameters to get the best results you can.
     # You may want to use different learning rates for each optimizer.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    wstd = 0.001
+    lr_vanilla = 0.1
+    lr_momentum = 0.1
+    lr_rmsprop = 0.1
+    reg = 0.5
     # ========================
     return dict(wstd=wstd, lr_vanilla=lr_vanilla, lr_momentum=lr_momentum,
                 lr_rmsprop=lr_rmsprop, reg=reg)
